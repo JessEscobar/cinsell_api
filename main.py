acdba8533b0e4119565140d1db2347f6fd59208d
Jessid Escobar <56373143+JessEscobar@users.noreply.github.com>
2020-12-22 01:05:02 -0500

    Nuevamente los cors

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
     "http://localhost", "http://localhost:8080","http://localhost:8080/perfil",
-    "http://127.0.0.1:8080", "http://127.0.0.1:8080/perfil","https://cinsell.herokuapp.com","https://cinsell.herokuapp.com/login","https://cinsell.herokuapp.com/users","https://cinsell.herokuapp.com/crear"
-    
+    "http://127.0.0.1:8080", "http://127.0.0.1:8080/perfil","https://cinsell.herokuapp.com/crear",
+    "https://cinsell.herokuapp.com/users","https://cinsell.herokuapp.com/login"
 ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
-
 @api.post("/login/")
 async def users_post(user_in:UserIn):
     user = get_user(user_in.username)
